82eeea3e87ae398d324e2a1a14bff85ae74be882
Anthony Castillo <anthony1093ca@gmail.com>
2024-04-27 14:13:40 -0700

    testing chat with RTX

---
+++ bot/test.py
@@ -0,0 +1,20 @@
+from time import time_ns
+import rtx_api_3_5 as rtx_api
+
+if __name__ == '__main__':
+    current_time_ns = time_ns()
+    start_sec = current_time_ns / 1_000_000_000
+
+    out = rtx_api.send_message("Write essay on: War, Famine, and Pestilence")
+
+    current_time_ns = time_ns()
+    end_sec = current_time_ns / 1_000_000_000
+
+    average_characters_in_token = 4
+    took = end_sec - start_sec
+    tokens_per_second = len(out) / took / average_characters_in_token
+    print("tokens/s:", int(tokens_per_second))
+
+    print(out + "\n")
+    out = rtx_api.send_message("Write single sentence sumary of: " + out)
+    print("SUMMARY: " + out)
