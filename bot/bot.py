ba23fe4b1ecbce43a5dd025cbf0307e6babb9a63
Anthony Castillo <anthony1093ca@gmail.com>
2024-04-27 20:57:53 -0700

    Parsed response to remove name of reference files

--- bot/bot.py
+++ bot/bot.py
@@ -1,4 +1,5 @@
 import bot.rtx_api_3_5 as rtx_api
+import string
 from threading import Thread
 
 class Bot():
@@ -16,6 +17,19 @@ class Bot():
         self.__threads.append(Thread(target=self.__history.append, name='input', args=[str(userInput)]))
         self.__threads[-1].start()
         response = rtx_api.send_message(userInput)
+        # We need to remove everything after, and including, <br>Reference files:<br>
+        word = ''
+        wordStart = 0
+        
+        for i, c in enumerate(response):
+            if word == 'br' and response[wordStart-1] == '<':
+                break
+            elif c == ' ' or c in string.punctuation:
+                word = ''
+                wordStart = i+1
+            else:
+                word += c
+        response = response[:wordStart-1]
         if self.__threads[-1].is_alive():
             self.__threads[-1].join()
         self.__threads.pop()
